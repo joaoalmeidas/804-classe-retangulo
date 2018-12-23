02fa88d97d5349bfc5566255541f4399af8f3121
joao <joao182cx@gmail.com>
2018-12-22 22:49:28 -0200

    Criação dos atributos altura e largura. Criação dos métodos construtores. Criação do método construtor que cria o objeto com o valor 1 para altura e largura.

--- src/Retangulo.java
+++ src/Retangulo.java
@@ -1,4 +1,19 @@
 
 public class Retangulo {
-
+	
+	private double altura;
+	private double largura;
+	
+	public Retangulo() {
+		this(1, 1);
+	}
+	
+	public Retangulo(double altura, double largura) {
+		super();
+		this.altura = altura;
+		this.largura = largura;
+	}
+	
+	
+	
 }
